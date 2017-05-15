b936d3cb6c52865ebd9b69d386515ce2e243fe82
Eric Jacobsson <eric.jacobsson@hiq.se>
2017-05-15 16:24:48 +0200

    added simple rest service

---
+++ src/main/java/com/airhacks/resources/SimpleRestService.java
@@ -0,0 +1,14 @@
+package com.airhacks.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/test")
+public class SimpleRestService {
+
+    @Path("/")
+    @GET
+    public String GetRootResource(){
+        return "hello";
+    }
+}
